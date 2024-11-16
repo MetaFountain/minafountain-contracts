44542227ed620bbbd5bc94f36499374ef7969374
Nasfame <laciferin2024@protonmail.com>
2024-11-17 03:22:47 +0700

    wip: Minafountain token

--- packages/hardhat/contracts/MinaFountain.sol
+++ packages/hardhat/contracts/MinaFountain.sol
@@ -3,12 +3,27 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
-contract MinaFountain is Ownable, ERC20, ERC20Pausable {
+contract MinaFountain is ERC20, ERC20Pausable, ERC20Permit {
 	uint256 public initialSupply = 1000000 * 10 ** decimals();
+	address public owner;
 
-	constructor() ERC20("MinaFountainToken", "MINAFOUNTAIN") {
+	constructor(
+		address initialOwner
+	)
+		Ownable(initialOwner)
+		ERC20("MinaFountainToken", "MINAFOUNTAIN")
+		ERC20Permit("MINAFOUNTAIN")
+	{
+		_mint(msg.sender, initialSupply);
+	}
+
+	constructor()
+		ERC20("MinaFountainToken", "MINAFOUNTAIN")
+		Ownable(msg.sender)
+	{
 		_mint(msg.sender, initialSupply);
 	}
 
@@ -20,11 +35,8 @@ contract MinaFountain is Ownable, ERC20, ERC20Pausable {
 		_unpause();
 	}
 
-	function _beforeTokenTransfer(
-		address from,
-		address to,
-		uint256 amount
-	) internal override(ERC20, ERC20Pausable) {
-		super._beforeTokenTransfer(from, to, amount);
+	modifier onlyOwner() {
+		require(msg.sender == owner, "Caller is not the owner");
+		_;
 	}
 }
